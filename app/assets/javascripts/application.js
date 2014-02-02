83ad40cc77d236ce34e8e3af2efdb2604a696808
landonmarder <landon.marder@gmail.com>
2014-02-02 17:28:40 -0500

    change to static map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,13 @@
 $(function(){ $(document).foundation(); });
 
 
-function initialize() {
-  var map_canvas = document.getElementById('map_canvas');
-  var map_options = {
-    center: new google.maps.LatLng(40.6651598,-73.5514469),
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(map_canvas, map_options)
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+// function initialize() {
+//   var map_canvas = document.getElementById('map_canvas');
+//   var map_options = {
+//     center: new google.maps.LatLng(40.6651598,-73.5514469),
+//     zoom: 16,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   }
+//   var map = new google.maps.Map(map_canvas, map_options)
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
