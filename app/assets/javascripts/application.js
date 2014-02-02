bf4cb63d0676f7fc926f0252fdee4155201394da
landonmarder <landon.marder@gmail.com>
2014-02-02 16:30:15 -0500

    add google maps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,15 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+
+function initialize() {
+  var map_canvas = document.getElementById('map_canvas');
+  var map_options = {
+    center: new google.maps.LatLng(40.6654836,-73.5515827),
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(map_canvas, map_options)
+}
+google.maps.event.addDomListener(window, 'load', initialize);
