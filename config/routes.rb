e585eec7a2efc449d77a5e01d609c33af6d503a9
landonmarder <landon.marder@gmail.com>
2016-06-17 22:25:45 -0400

    add virtual showroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Ssht::Application.routes.draw do
-get '/' => 'pages#home'
-get '/about' => 'pages#about'
-get '/contact' => 'pages#contact'
-
+  get '/' => 'pages#home'
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
+  get '/store' => "pages#store"
 end
